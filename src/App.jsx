ba68287ec5d23d51e73d7674475603ef832da1bd
Thiago Puntar <thiagopuntar@gmail.com>
2024-10-14 19:41:31 -0300

    add context for currentUser

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import Navigation from "./components/Navigation";
-import avatarMock from "./images/avatar_mock.png";
-import editIcon from "./images/edit.svg";
 import PopupWithForm from "./components/PopupWithForm";
 import Card from "./components/Card";
 import { useState, useEffect } from "react";
 import api from "./utils/api";
+import { CurrentUserContext } from "./contexts/CurrentUserContext";
+import Seller from "./components/Seller";
 
 function App() {
-  // array destructuring
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isProductPopupOpen, setIsProductPopupOpen] = useState(false);
-  const [userInfo, setUserInfo] = useState({});
   const [cards, setCards] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
     api.getUserInfo().then((apiUserInfo) => {
-      setUserInfo(apiUserInfo);
+      setCurrentUser(apiUserInfo);
     });
   }, []);
 
@@ -39,130 +38,110 @@ function App() {
   };
 
   return (
-    <div className="page">
-      <main className="container">
-        <Navigation />
-        <div className="seller">
-          <article className="seller__card">
-            <figure>
-              <img
-                src={avatarMock}
-                alt="Seller avatar image"
-                className="seller__avatar"
-              />
-            </figure>
-            <div className="seller__data">
-              <h2 className="seller__name">{userInfo.name}</h2>
-              <h3 className="seller__job">{userInfo.job}</h3>
-            </div>
-            <button className="seller__edit" onClick={handleSellerClick}>
-              <img
-                src={editIcon}
-                alt="Edit seller data"
-                className="seller__edit-icon"
-              />
-              <span className="seller__edit-label">Editar</span>
-            </button>
-          </article>
-        </div>
-        <ul className="products">
-          {cards.map((item) => {
-            return <Card key={item._id} data={item} />;
-          })}
-        </ul>
-        <div className="cta">
-          <button className="button button_type_primary cta_product_edit">
-            Editar itens
-          </button>
-          <button
-            className="button button_type_success cta_product_add"
-            onClick={handleAddProductClick}
-          >
-            Adicionar item
-          </button>
-        </div>
-        <aside className="order mobile-hidden" id="order">
-          <div className="order__header">
-            <p className="order__title">Resumo da Venda</p>
-            <p className="order__author">José Costa</p>
-          </div>
-          <ul className="order__list">
-            <template id="order__entry" />
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <main className="container">
+          <Navigation card={cards} />
+          <Seller onEditProfile={handleSellerClick} />
+          <ul className="products">
+            {cards.map((item) => {
+              return <Card key={item._id} data={item} />;
+            })}
           </ul>
-          <div className="order__total-container">
-            <div>
-              <span className="order__total-text-bold">Sub total</span>
-              <span className="order__total-text">Descontos</span>
-              <span className="order__total-text-bold">Total</span>
-            </div>
-            <div>
-              <span className="order__total order__total-text-bold">
-                R$ 0,00
-              </span>
-              <span className="order__total-text">R$ 0,00</span>
-              <span className="order__subtotal order__total-text-bold">
-                R$ 0,00
-              </span>
-            </div>
+          <div className="cta">
+            <button className="button button_type_primary cta_product_edit">
+              Editar itens
+            </button>
+            <button
+              className="button button_type_success cta_product_add"
+              onClick={handleAddProductClick}
+            >
+              Adicionar item
+            </button>
           </div>
-          <button
-            className="button button_type_success order__button"
-            id="order__button"
-          >
-            Finalizar
-          </button>
-        </aside>
-      </main>
-      <PopupWithForm
-        id="popup_edit_profile"
-        title="Editar Perfil"
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}
-      >
-        <div className="form__control">
-          <label className="form__label" htmlFor="name">
-            Nome:
-          </label>
-          <input className="form__input" name="name" id="name" required="" />
+          <aside className="order mobile-hidden" id="order">
+            <div className="order__header">
+              <p className="order__title">Resumo da Venda</p>
+              <p className="order__author">José Costa</p>
+            </div>
+            <ul className="order__list">
+              <template id="order__entry" />
+            </ul>
+            <div className="order__total-container">
+              <div>
+                <span className="order__total-text-bold">Sub total</span>
+                <span className="order__total-text">Descontos</span>
+                <span className="order__total-text-bold">Total</span>
+              </div>
+              <div>
+                <span className="order__total order__total-text-bold">
+                  R$ 0,00
+                </span>
+                <span className="order__total-text">R$ 0,00</span>
+                <span className="order__subtotal order__total-text-bold">
+                  R$ 0,00
+                </span>
+              </div>
+            </div>
+            <button
+              className="button button_type_success order__button"
+              id="order__button"
+            >
+              Finalizar
+            </button>
+          </aside>
+        </main>
+        <PopupWithForm
+          id="popup_edit_profile"
+          title="Editar Perfil"
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <div className="form__control">
+            <label className="form__label" htmlFor="name">
+              Nome:
+            </label>
+            <input className="form__input" name="name" id="name" required="" />
+          </div>
+          <div className="form__control">
+            <label className="form__label" htmlFor="about">
+              Sobre:
+            </label>
+            <input className="form__input" id="about" name="job" required="" />
+          </div>
+        </PopupWithForm>
+        <PopupWithForm
+          id="popup_add_product"
+          title="Novo Produto"
+          isOpen={isProductPopupOpen}
+          onClose={closeAllPopups}
+        >
+          <div className="form__control">
+            <label className="form__label" htmlFor="image">
+              Imagem:
+            </label>
+            <input className="form__input" id="image" />
+          </div>
+          <div className="form__control">
+            <label className="form__label" htmlFor="productName">
+              Nome:
+            </label>
+            <input className="form__input" name="name" id="productName" />
+          </div>
+          <div className="form__control">
+            <label className="form__label" htmlFor="price">
+              Preço:
+            </label>
+            <input className="form__input" id="price" />
+          </div>
+        </PopupWithForm>
+        <div className="popup popup_image">
+          <button className="popup__close-button">X</button>
+          <img src="" alt="" className="popup__image" />
+          <p className="popup__image-text" />
         </div>
-        <div className="form__control">
-          <label className="form__label" htmlFor="about">
-            Sobre:
-          </label>
-          <input className="form__input" id="about" name="job" required="" />
-        </div>
-      </PopupWithForm>
-      <PopupWithForm
-        id="popup_add_product"
-        title="Novo Produto"
-        isOpen={isProductPopupOpen}
-        onClose={closeAllPopups}
-      >
-        <div className="form__control">
-          <label className="form__label" htmlFor="image">
-            Imagem:
-          </label>
-          <input className="form__input" id="image" />
-        </div>
-        <div className="form__control">
-          <label className="form__label" htmlFor="productName">
-            Nome:
-          </label>
-          <input className="form__input" name="name" id="productName" />
-        </div>
-        <div className="form__control">
-          <label className="form__label" htmlFor="price">
-            Preço:
-          </label>
-          <input className="form__input" id="price" />
-        </div>
-      </PopupWithForm>
-      <div className="popup popup_image">
-        <button className="popup__close-button">X</button>
-        <img src="" alt="" className="popup__image" />
-        <p className="popup__image-text" />
       </div>
-    </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Seller.jsx
+++ src/components/Seller.jsx
@@ -1,7 +1,11 @@
 import avatarMock from "../images/avatar_mock.png";
 import editIcon from "../images/edit.svg";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-export default function Seller() {
+export default function Seller({ onEditProfile }) {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <div className="seller">
       <article className="seller__card">
@@ -13,10 +17,10 @@ export default function Seller() {
           />
         </figure>
         <div className="seller__data">
-          <h2 className="seller__name" />
-          <h3 className="seller__job" />
+          <h2 className="seller__name">{currentUser.name}</h2>
+          <h3 className="seller__job">{currentUser.job}</h3>
         </div>
-        <button className="seller__edit">
+        <button className="seller__edit" onClick={onEditProfile}>
           <img
             src={editIcon}
             alt="Edit seller data"
